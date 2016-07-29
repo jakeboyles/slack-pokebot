dfaac37fa7b066a92759c2709aa6dc87f709fd72
Jake Boyles <jake@jibdesigns.com>
2016-07-28 20:13:06 -0400

    adding images from google

--- index.js
+++ index.js
@@ -1,128 +1,183 @@
-'use strict';
-
 var PokemonGO = require('pokemon-go-node-api');
 var request = require('request');
-var geolib = require('geolib');
 var _ = require('lodash');
 
+var logger = require('./logger');
 var metrics = require('./metrics');
+var geo = require('./geo');
 
-var winston;
-if ( process.env.LOGGLY_TOKEN ){
-  winston = require('winston');
-  require('winston-loggly-bulk');
-  winston.add(winston.transports.Loggly, {
-    token: process.env.LOGGLY_TOKEN,
-    subdomain: process.env.LOGGLY_SUBDOMAIN,
-    tags: ["Winston-NodeJS"],
-    json: true
-  });
-}else{
-  winston = {
-    log   : function(type,msg){ console.log(type+"\t: "+msg); },
-    error : function(msg){ console.log("E\t: "+msg); }
-  }
-}
-
-winston.log('info',"Initialised");
+logger.log('info',"Initialised");
 
 var a = new PokemonGO.Pokeio();
 
 var location = {
   type: 'name',
-  name:  '39.326539499999996,-84.4218144',
+  name: process.env.PGO_LOCATION || "new york",
 };
+var geoLocation = process.env.PGO_LOCATION.match(/^(-?\d+\.\d+),(-?\d+\.\d+)$/);
+if ( geoLocation ){
+  location.type = 'coords';
+  location.coords = {
+    latitude:parseFloat(geoLocation[1]),
+    longitude:parseFloat(geoLocation[2]),
+    altitude:0.0
+  }
+}
 
-var username = process.env.PGO_USERNAME || 'jacobboyles';
-var password = process.env.PGO_PASSWORD || 'baseball200';
+var username = process.env.PGO_USERNAME || "jacobboyles";
+var password = process.env.PGO_PASSWORD || "baseball200";
 var provider = process.env.PGO_PROVIDER || 'ptc';
 
-var pokeMap = {};
+var start_location;
 
 a.init(username, password, location, provider, function(err) {
-  if (err) console.log(err);
+  if (err){
+    logger.error(err);
+    process.exit(2);
+  }
 
-  winston.log('info', 'Current location: ' + a.playerInfo.locationName);
-  winston.log('info', 'lat/long/alt: : ' + a.playerInfo.latitude + ' ' + a.playerInfo.longitude + ' ' + a.playerInfo.altitude);
-  var start_location = {latitude:a.playerInfo.latitude,
-    longitude:a.playerInfo.longitude};
+  logger.log('info', 'Current location: ' + a.playerInfo.locationName);
+  logger.log('info', 'lat/long/alt: : ' + a.playerInfo.latitude + ' ' + a.playerInfo.longitude + ' ' + a.playerInfo.altitude);
+  start_location = {
+    latitude:a.playerInfo.latitude,
+    longitude:a.playerInfo.longitude };
 
   a.GetProfile(function(err, profile) {
-    if (err) throw err;
+    if (err){
+      logger.error(err);
+      process.exit(3);
+    }
 
-    winston.log('info', 'Username: ' + profile.username);
-    // console.log('1[i] Poke Storage: ' + profile.poke_storage);
-    // console.log('1[i] Item Storage: ' + profile.item_storage);
-    //
-    // var poke = 0;
-    // if (profile.currency[0].amount) {
-    //   poke = profile.currency[0].amount;
-    // }
-    //
-    // console.log('1[i] Pokecoin: ' + poke);
-    // console.log('1[i] Stardust: ' + profile.currency[1].amount);
+    logger.log('info', 'Username: ' + profile.username);
 
-    setInterval(function() {
+    function getHeartbeat() {
+      logger.log('info','Requesting heartbeat');
       a.Heartbeat(function (err,hb) {
         if(err) {
-          winston.log('error', err);
+          logger.error(err);
+          process.exit(3);
         }
 
         if (!hb || !hb.cells) {
-          winston.log('error', 'hb or hb.cells undefined - aborting');
+          logger.error('hb or hb.cells undefined - aborting');
         } else {
+          logger.log('info', 'Heartbeat received');
+          var encounters = {};
           for (var i = hb.cells.length - 1; i >= 0; i--) {
             if(hb.cells[i].WildPokemon[0]) {
               var wildPokemon = hb.cells[i].WildPokemon;
-              var newPokeMap = {};
               for (var j = wildPokemon.length - 1; j >= 0; j--) {
                 var pokeId = wildPokemon[j].pokemon.PokemonId;
                 var pokemon = a.pokemonlist[parseInt(pokeId)-1];
-                newPokeMap[ pokemon.id ] = true;
-
-                var pokemonAlreadyPresent = pokeMap[ pokemon.id ];
-
-                if (!pokemonAlreadyPresent) {
-                  var latitude = wildPokemon[j].Latitude;
-                  var longitude = wildPokemon[j].Longitude;
-
-                  var position = { latitude : wildPokemon[j].Latitude,
-                    longitude : wildPokemon[j].Longitude};
-                  var distance = geolib.getDistance(position,start_location)
-                  if ( true == true ){
-                    let image = `https://maps.googleapis.com/maps/api/staticmap?center=${position.latitude},${position.longitude}&size=640x400&style=element:labels|visibility:off&style=element:geometry.stroke|visibility:off&style=feature:landscape|element:geometry|saturation:-100&style=feature:water|saturation:-100|invert_lightness:true&key=AIzaSyBmhVz0j9QcBbHfYtusRMQfjSELV24gLkc&zoom=14&&markers=color:blue%7Clabel:S%7C${position.latitude},${position.longitude}`;
-                    var message = 'There is a *' + pokemon.name + '* ('+pokemon.num+') '+distance+'m away! <https://maps.google.co.uk/maps?f=d&dirflg=w&saddr=' + start_location.latitude+","+start_location.longitude+'&daddr=' + position.latitude + ',' + position.longitude+'|Route>';
-                    if ( true == true ){
-                      request.post({
-                        url: "https://hooks.slack.com/services/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",
-                        json: true,
-                        body: {
-                          text: message,
-                          icon_url: pokemon.img,
-                          "attachments": [
-                            {
-                            "image_url":image,
-                            }
-                          ]
-                        }
-                      }, function(error, response, body) {
-                        if (error) winston.log('error', error);
-                        if(response.body) console.log(response.body);
-                      });
-                    }
-                    winston.log('info', "POST: "+ message );
-                  } else {
-                    winston.log('info', pokemon.name + ' not interesting: skipping');
-                  }
-                } else {
-                  winston.log('info', pokemon.name + ' already present: skipping');
-                }
+                var position = { latitude : wildPokemon[j].Latitude,
+                                 longitude : wildPokemon[j].Longitude};
+                var encounterId = wildPokemon[j].SpawnPointId;
+                encounters[encounterId]= { pokemon:pokemon , details:wildPokemon[j], position:position };
               }
-              pokeMap = newPokeMap;
             }
           }
+          var hbPokemon = [];
+          for ( var key in encounters ){
+            hbPokemon.push(encounters[key]);
+          }
+          logger.log('info','Found '+hbPokemon.length+' pokemon');
+          if ( hbPokemon.length == 0 ) return;
+          var newPokemon = removeKnownPokemon( hbPokemon );
+          logger.log('info','Found '+newPokemon.length+' new pokemon');
+          if ( newPokemon.length == 0 ) return;
+          var interestingPokemon = removeUninteretingPokemon( newPokemon );
+          logger.log('info','Found '+interestingPokemon.length+' interesting pokemon');
+          if ( interestingPokemon.length == 0 ) return;
+          sendMessage( interestingPokemon );
         }
       });
-    }, 60000);
+    }
+    getHeartbeat();
+    setInterval( getHeartbeat , 60000);
   });
 });
+
+
+var knownPokemon = {};
+function removeKnownPokemon(pokemon){
+  var nextKnownPokemon = {};
+  var unknownPokemon = [];
+  for ( var id in pokemon ){
+    var p = pokemon[id];
+    if ( !knownPokemon[p.details.SpawnPointId] ){
+      unknownPokemon.push(p);
+    }
+    nextKnownPokemon[p.details.SpawnPointId] = true;
+  }
+  knownPokemon = nextKnownPokemon;
+  return unknownPokemon;
+}
+
+function removeUninteretingPokemon(pokemon){
+  var interestingPokemon = [];
+  for ( var id in pokemon ){
+    var p = pokemon[id];
+    p.distance = geo.getDistance(p.position,start_location);
+    p.bearing = geo.cardinalBearing(geo.getBearing(start_location,p.position));
+    if ( metrics.shouldReport( p ) ){
+      interestingPokemon.push(p);
+    }
+  }
+  return interestingPokemon
+}
+
+function sendMessage(pokemon){
+  for ( var id in pokemon ){
+    postPokemonMessage(pokemon[id]);
+  }
+}
+
+function postPokemonMessage(p){
+    var pre = "";
+    if ( p.rarity.match(/rare/i) ) pre = "@here ";
+    geo.reverseGeoCode(p.position, function(geocode){
+      var seconds = Math.floor(p.details.TimeTillHiddenMs / 1000);
+      var remaining = Math.floor(seconds/60)+":"+Math.floor(seconds%60)+" remaining";
+      var message = pre+'A wild *' + p.pokemon.name + '* appeared!\n' +
+                    '<https://maps.google.co.uk/maps?f=d&dirflg=w&'+
+                    'saddr='+start_location.latitude+","+start_location.longitude+'&'+
+                    'daddr='+p.position.latitude+','+p.position.longitude+'|'+p.distance+'m '+p.bearing+geocode + ')>\n' +
+        remaining;
+
+      var COLOUR_BY_RARITY = {
+        "common": "#19A643",
+        "uncommon": "#1BC4B9",
+        "rare": "#1E0BE6",
+        "ultra-rare": "#E600FF"
+      };
+
+      if(process.env.MAPS_API)
+      {
+        image = `https://maps.googleapis.com/maps/api/staticmap?center=${position.latitude},${position.longitude}&size=640x400&style=element:labels|visibility:off&style=element:geometry.stroke|visibility:off&style=feature:landscape|element:geometry|saturation:-100&style=feature:water|saturation:-100|invert_lightness:true&key=AIzaSyBmhVz0j9QcBbHfYtusRMQfjSELV24gLkc&zoom=14&&markers=color:blue%7Clabel:S%7C${p.position.latitude},${p.position.longitude}`;
+      }
+       if ( process.env.SLACK_WEBHOOK_URL ){
+        request.post({
+          url: process.env.SLACK_WEBHOOK_URL,
+          json: true,
+          body: {
+            attachments: [
+              {
+                "fallback": message,
+                "color": COLOUR_BY_RARITY[p.rarity],
+                "image_url": p.pokemon.img,
+                "text": message,
+                "unfurl_media": true
+              },
+              {
+                "image_url": image,
+              }
+            ]
+          }
+        }, function(error, response, body) {
+          if(error) logger.error(error);
+          if(response.body) logger.log(response.body);
+        });
+      }
+      logger.log('info', "POST: "+ message );
+    });
+}
